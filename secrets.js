8b1d3c0ab64fbe1075499d6f5c4ebbbdd1635406
Ernesto <egonzalez442@gmail.com>
2022-12-23 20:46:09 -0500

    fixed deprecated issues in server.js & added secrets folder and babelrc folder

---
+++ secrets.js
@@ -0,0 +1,9 @@
+module.exports = {
+  /* The password for the database. */
+  DB_PASSWORD: 'dAQs98txSZ_ucoRBYsUZYxwopArHE2ls',
+  GOOGLE_CLIENT_ID:
+    '483750040191-lm1ckhp9spqbv469t5qvso11pgj6gupl.apps.googleusercontent.com',
+  GOOGLE_CLIENT_SECRET: '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',
+  SESSION_SECRET: 'OAGDGLK2T4GWKSD09253LKF2457KITRSL',
+  DEBUG: false,
+};
