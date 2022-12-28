89d109ed6f331314e84ddf92e3798907922a1ec7
Michael Chiang <michael.chiang.mc5@gmail.com>
2022-12-28 12:58:38 -0800

    added endpoint to create a card Tested with postman

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,53 @@ const pool = require('./pool.js');
 
 const obj = {};
 
-obj.getCards = async () => {
-  const sql = 'SELECT * FROM cards';
-  const data = await pool.query(sql);
-  return data.rows;
+// obj.readCard = async (id) => {
+//   const sql = `SELECT *
+//   FROM Cards
+//   WHERE _id=$1;`;
+//   const data = await pool.query(sql, [id]);
+//   // TODO: validate that there is only one row
+//   console.log(data.rows[0]);
+//   return data.rows[0];
+// };
+
+// obj.readAllCards = async () => {
+//   const sql = `SELECT *
+//   FROM Cards;`;
+//   const data = await pool.query(sql);
+//   return data.rows;
+// };
+
+obj.createCard = async (args) => {
+  try {
+    // this is the current time in format 2022-12-28 12:34:56
+    const currentTime = new Date();
+    const formattedTime = currentTime
+      .toISOString()
+      .slice(0, 19)
+      .replace('T', ' ');
+
+    // parameterize sql arguments to prevent attacks
+    const arr = [
+      Number(args['user_id']),
+      args['title'],
+      args['front'],
+      args['back'],
+      Number(args['difficulty']),
+      args['hints'],
+      args['scheduled'] === null ? formattedTime : args['scheduled'], // args['scheduled'] should have format 2022-12-28 12:34:56
+    ];
+
+    const sql = `INSERT INTO Cards
+    (user_id, title, front, back, difficulty, hints, scheduled)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *;`;
+    // execute sql command
+    const data = await pool.query(sql, arr);
+    return data.rows[0];
+  } catch (err) {
+    console.log('createCard', err);
+  }
 };
 
 obj.addUser = async (args) => {
--- src/server/apiRouter.js
+++ src/server/apiRouter.js
@@ -2,9 +2,24 @@ const db = require('../db/db.js');
 const express = require('express');
 const router = express.Router();
 
-router.post('/db', async (req, res) => {
-  const rn = await db.getCards();
-  res.status(200).send(rn);
+router.post('/cards', async (req, res) => {
+  // sanitize post data
+  const data = {
+    user_id: null,
+    title: null,
+    front: null,
+    back: null,
+    difficulty: null,
+    hints: null,
+    scheduled: null,
+  };
+  for (let key in data) {
+    const value = req.body[key];
+    data[key] = value === undefined ? null : value;
+  }
+  console.log('creating data: ', data);
+  const row = await db.createCard(data);
+  res.status(200).json(row);
 });
 
 module.exports = router;
