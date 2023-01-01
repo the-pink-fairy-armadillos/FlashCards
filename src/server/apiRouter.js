cd11bb581ed25443cf74dc13209b95776cf5b156
Your Name <grakina.production@gmail.com>
2023-01-01 16:41:51 -0500

    project update -alina

--- src/server/apiRouter.js
+++ src/server/apiRouter.js
@@ -7,4 +7,30 @@ router.post('/db', async (req, res) => {
   res.status(200).send(rn);
 });
 
+router.update('/cards/:id', async (req, res, next) => {
+  try {
+    const { user_id, title, front, back, difficulty, hints, scheduled } = req.body; 
+
+  } catch(err) {
+    next({
+      log: 'error updating the card', 
+      status: 500, 
+      message: { err: err }, 
+    }); 
+  }
+})
+
+router.delete('/cards/:id', async (req, res, next) => {
+  try {
+      // const {user_id} = req.body
+      // let query = 'DELETE FROM cards WHERE '
+  } catch(err) {
+    next({
+      log: 'error deleting the card', 
+      status: 500, 
+      message: { err: err }, 
+    }); 
+  }
+})
+
 module.exports = router;
