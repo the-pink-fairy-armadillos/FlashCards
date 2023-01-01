23c28fe7e62d29e81954cf103b6038070537c085
Your Name <grakina.production@gmail.com>
2023-01-01 18:53:24 -0500

    project update 2 -alina

--- src/db/db.js
+++ src/db/db.js
@@ -2,6 +2,40 @@ const pool = require('./pool.js');
 
 const obj = {};
 
+obj.updateCard = async (args) => {
+  try {
+    
+    const sql1 = ` SELECT * FROM Cards WHERE _id=$1`;
+    const data1 = await pool.query(sql1, Number(args['_id']));
+
+    const arr = [
+      Number(args['_id']),
+      args['user_id'] === undefined ? data1.rows[0][1] : args['user_id'],
+      args['title'] === undefined ? data1.rows[0][2] : args['title'],
+      args['front'] === undefined ? data1.rows[0][3] : args['front'],
+      args['back'] === undefined ? data1.rows[0][4] : args['back'],
+      Number(args['difficulty']) === undefined ? data1.rows[0][5] : args['difficulty'],
+      args['hints'] === undefined ? data1.rows[0][6] : args['hints'],
+      args['scheduled'] === undefined ? data1.rows[0][7] : args['scheduled'],
+    ];
+
+    const sql2 = ` UPDATE Cards
+    SET user_id = $2, 
+    title = $3,
+    front = $4,
+    back = $5,
+    difficulty = $6,
+    hints = $7,
+    scheduled = $8
+    WHERE _id = $1`;
+
+    const data2 = await pool.query(sql2, arr);
+
+  } catch (err) {
+    throw `In db.js: obj.updateCard: ${err.message}`; 
+  }
+}
+
 obj.getCards = async () => {
   const sql = 'SELECT * FROM cards';
   const data = await pool.query(sql);
