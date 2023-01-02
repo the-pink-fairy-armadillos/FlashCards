60a11995e03c86f99de4829d92376f9145db065b
Your Name <grakina.production@gmail.com>
2023-01-02 14:12:34 -0800

    alina changes in flashcard, styled/centered the cards

--- src/client/Components/ShowCard/FlashCards.js
+++ src/client/Components/ShowCard/FlashCards.js
@@ -4,12 +4,15 @@ import styles from './flashCard.module.css';
 import { useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
 import { json, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const FlashCard = () => {
   const { id } = useParams(); 
   console.log(id); 
   const [cardData, setCardData] = useState({});
+  const [nextCard, setnextCard] = useState({});
   const [showFront, setShowFront] = useState(false);
+  const navigate = useNavigate();
 
 
   useEffect(() => {
@@ -23,21 +26,36 @@ const FlashCard = () => {
       setCardData(res.data);
     });
   }, []);
+
+  useEffect(() => {
+    // we cannot use async/await in useEffect without wrapping in outer function
+    const response = axios({
+      method: 'get',
+      withCredentials: true,
+      url: `http://localhost:8080/api/cards/nextCard/${id}`,
+    }).then((res) => {
+      console.log(res.data)
+      console.log('checking for res.data', res.data)
+      setnextCard(res.data);
+    });
+  }, []);
   
   return (
     <>
 
-      <div className="container d-flex justify-content-center">
+      <div className="container d-flex justify-content-center text-center">
         <div className="col">
           <div id="card-title-wrapper" className={`${styles.containerbox}`}>
-            <h1 className={`${styles.title}`}>{cardData.title}</h1>
+            <h1 className={`${styles.title}`}>{cardData.title ?? ' No Title'}</h1>
           </div>
 
           <div id="card-frontCard" onClick={() => setShowFront(!showFront)} className={`${styles.containerbox2}` }>
-            <p className="title">
+            <p className={`${styles.paragraph}`}>
               {showFront ? cardData.front : cardData.back}
             </p>
          </div>
+
+         <button onClick={() => window.location.href=(`/flashcard/${nextCard}`) } className={`${styles.addCardBtn}`}>NEXT CARD</button>
 
         </div>
       </div>
