22f9d6d8df3a041b364858053f62087f5c804f0c
Your Name <grakina.production@gmail.com>
2023-01-02 10:41:22 -0800

    alina creted flash card component - not connected to backend yet

--- src/client/Components/Show Card/FlashCards.js
+++ src/client/Components/Show Card/FlashCards.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import styles from './flashcard.module.css';
+import styles from './flashCard.module.css';
 
 const FlashCard = () => {
   return (
